ece97b81ad60c4e9a0a3b1de7d8396e594844754
dream6-e <15606442582@163.com>
2026-05-28 01:46:35 +0800

    Delete Yeskid.lua

--- Yeskid.lua
+++
@@ -1,3 +0,0 @@
-getgenv().SCRIPT_KEY = "QUITE.GG-f611aae8-a933-42f8-bd0e-944926d62480"
-
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/9fc57bc1857ec6101bb11cfc8ab5506335593e171e62cdc6ea6150dec9957034/download"))()
